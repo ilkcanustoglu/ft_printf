1e953299c15c39340fd48a549abc4c878d1e17ba
ilkcanustoglu <ilkcanustoglu@gmail.com>
2022-04-20 14:10:38 +0300

    ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iustoglu <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 18:06:50 by iustoglu          #+#    #+#             */
+/*   Updated: 2022/03/15 18:07:05 by iustoglu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+# include <stdlib.h>
+
+int		ft_putstr(char *str);
+int		ft_printf(const char *sign, ...);
+int		ft_putchar(char c);
+int		ft_putpointer(void *ptr);
+int		ft_putnbr(int number);
+int		ft_putunint(unsigned int number);
+char	*ft_convert(size_t num, char *set);
+
+#endif
